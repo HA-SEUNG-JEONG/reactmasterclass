34da84c8de8be1bccf49dd6959945ee7623023c1
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-12-02 12:13:35 +0900

    'As' and Attrs

--- App.js
+++ App.js
@@ -4,22 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-//Box 변수를 상속받음
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, maxLength: 10 })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Circle bgColor="greenyellow" />
-      <Circle bgColor="red" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
