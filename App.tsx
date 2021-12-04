e49830e8922a73ca2c6bf2088e98083e58647c28
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-12-04 14:24:22 +0900

    Typing the Props

--- App.tsx
+++ App.tsx
@@ -1,34 +1,11 @@
-import styled from "styled-components";
+import Circle from "./Circle";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 function App() {
   return (
-    <Wrapper>
-      <Box></Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
