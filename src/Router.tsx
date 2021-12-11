c95d868f2efff37226dabf4287acc8d2de6d4028
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-12-11 21:49:44 +0900

    HashRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 
 function Router() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Switch>
         <Route path="/:coinId">
           <Coin />
@@ -13,7 +13,7 @@ function Router() {
           <Coins />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
